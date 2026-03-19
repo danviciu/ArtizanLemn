ebaada8defa44d51851981843f1f87c4c64dbf6d
Dan Voicu <danvoicuteodorescu@gmail.com>
2026-03-19 09:16:17 +0200

    Add favicon fallback files for Google and browsers

--- lib/site.ts
+++ lib/site.ts
@@ -69,14 +69,33 @@ export const defaultMetadata: Metadata = {
   icons: {
     icon: [
       {
+        url: "/favicon.ico",
+        type: "image/x-icon",
+      },
+      {
+        url: "/favicon-48x48.png",
+        sizes: "48x48",
+        type: "image/png",
+      },
+      {
+        url: "/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+      {
+        url: "/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+      {
         url: "/icon.svg",
         type: "image/svg+xml",
       },
     ],
     shortcut: [
       {
-        url: "/icon.svg",
-        type: "image/svg+xml",
+        url: "/favicon.ico",
+        type: "image/x-icon",
       },
     ],
     apple: [
